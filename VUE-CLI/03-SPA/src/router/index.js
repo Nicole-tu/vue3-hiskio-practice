2a4efad188fae7c85f2439e618dbd544e4c53064
Tu Nicole <nicole@TudeMacBook-Pro.local>
2020-11-26 15:13:02 +0800

    router setting

--- VUE-CLI/03-SPA/src/router/index.js
+++ VUE-CLI/03-SPA/src/router/index.js
@@ -1,6 +1,6 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
-import Rwd from "../views/Rwd.vue";
+// import Rwd from "../views/Rwd.vue";
 
 const routes = [
   {
@@ -14,6 +14,8 @@ const routes = [
     // 網站一開始的時候就載入全部的內容，會造成loading時間過久
     // component: Rwd,
     // lazy loading (dynamic loading)進入router時才會載入內容，加快載入時間
+    // 動態載入的元件會被打包成獨立的js 增加request時間
+    // 所以要依照頁面的複雜程度決定是不是要動態載入
     component: () => import("../views/Rwd.vue"),
 
   },
@@ -40,7 +42,12 @@ const routes = [
 ];
 
 const router = createRouter({
+  // history:router切換模式
+  // 要請後端設定router的指向位置到根目錄
   history: createWebHistory(process.env.BASE_URL),
+  // 可以用#切換頁面，如果網頁也有錨點的機制會讓網站錯誤，SEO也會下降，爬蟲不會抓到#後面的部
+  // 後台網站不care SEO及網址長相建議使用createWebHashHistory
+  // history: createWebHashHistory(), 
   routes,
 });
 
