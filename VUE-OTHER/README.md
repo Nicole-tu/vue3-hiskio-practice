4e7668d2af54cff7ecd258045444b11947d33360
Tu Nicole <nicole@TudeMacBook-Pro.local>
2020-12-06 21:25:17 +0800

    vue other

---
+++ VUE-OTHER/README.md
@@ -0,0 +1,21 @@
+# 其他vue官方推薦的新功能
+1. script setup
+2. style css variable injection
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ VUE-OTHER/src/App.vue
@@ -0,0 +1,50 @@
+<!--script 
+// import { ref } from "vue";
+// export default {
+//   setup() {
+//     const name = ref("mike");
+//     return {
+//       name,
+//     };
+//   },
+// };
+/script -->
+<!-- 
+1. script setup
+在template裡面沒有使用到的變數就不會被return ex:timer
+注意！：其他編譯工具可能沒有支援這個寫法
+-->
+<!-- 
+2. style CSS variable injection
+宣告CSS變數可以直接在style裡面用v-bind變數
+-->
+<script setup>
+import { ref } from 'vue';
+
+let timer = ref(0);
+
+const name = ref('mike');
+const handleName = () => {
+  name.value = 'andy';
+}
+
+const nameColor = ref('#FF0000');
+const handleColor = () => {
+  nameColor.value = '#0000FF';
+}
+
+</script>
+
+<template>
+  <h1 class="title">{{ name }}</h1>
+  <button @click="handleName">change name</button>
+  <button @click="handleColor">change color</button>
+</template>
+
+<style lang="scss">
+.title {
+  font-size: 30px;
+  // color: red;
+  color: v-bind(nameColor);
+}
+</style>
